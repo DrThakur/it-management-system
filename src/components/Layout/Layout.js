e5c15f4ef3f4aac174477cd509679b00180af13f
Ankit Kumar Thakur <drankitkumarthakur@gmail.com>
2023-06-30 15:07:23 +0530

    New components of table added to dashboard

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -43,6 +43,97 @@
 
 // export default Layout;
 
+// import React, { useState } from "react";
+// import Footer from "../Footer/Footer";
+// import Header from "../Header/Header";
+// import Sidebar from "../Sidebar/Sidebar";
+// import NotificationMenu from "../Notification/Notification";
+
+// const Layout = ({ children }) => {
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+//   const [isNotificationMenuOpen, setIsNotificationMenuOpen] = useState(true);
+
+//   const toggleSidebar = () => {
+//     setIsSidebarOpen(!isSidebarOpen);
+//   };
+
+//   const toggleNotificationMenu = () => {
+//     setIsNotificationMenuOpen(!isNotificationMenuOpen);
+//   };
+
+//   return (
+//     <div className="flex flex-col min-h-screen">
+//       <div className="flex flex-grow">
+//         {isSidebarOpen && <Sidebar />}
+//         <div className="flex flex-col flex-grow relative">
+//           <Header
+//             toggleSidebar={toggleSidebar}
+//             toggleNotificationMenu={toggleNotificationMenu}
+//           />
+//           <div className="flex-grow p-4">
+//             <div className="flex flex-grow">
+//               {isNotificationMenuOpen && <NotificationMenu />}
+//               <div className="flex flex-col flex-grow relative">{children}</div>
+//             </div>
+//           </div>
+//           <Footer />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
+// import React, { useState } from "react";
+// import Footer from "../Footer/Footer";
+// import Header from "../Header/Header";
+// import Sidebar from "../Sidebar/Sidebar";
+// import NotificationMenu from "../Notification/Notification";
+
+// const Layout = ({ children }) => {
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+//   const [isNotificationMenuOpen, setIsNotificationMenuOpen] = useState(true);
+
+//   const toggleSidebar = () => {
+//     setIsSidebarOpen(!isSidebarOpen);
+//   };
+
+//   const toggleNotificationMenu = () => {
+//     setIsNotificationMenuOpen(!isNotificationMenuOpen);
+//   };
+
+//   return (
+//     <div className="flex flex-col min-h-screen">
+//       {/*Layout div */}
+//       <div className="flex flex-grow">
+//         {/*Sidebar div */}
+//         {isSidebarOpen && <Sidebar />}
+//         <div className="flex flex-col flex-grow relative">
+//           {/*Header and Footer div */}
+//           <Header
+//             toggleSidebar={toggleSidebar}
+//             toggleNotificationMenu={toggleNotificationMenu}
+//           />
+//           <div className="flex-grow p-4">
+//             {/*Content + Right  div */}
+//             <div className="flex flex-col flex-grow relative">{children}</div>
+//             <div className="flex flex-grow">
+//               {isNotificationMenuOpen && <NotificationMenu />}
+//             </div>
+//           </div>
+//           {/*Content + Right  div closing*/}
+//           <Footer />
+//         </div>
+//         {/*Header and Footer div  closing*/}
+//       </div>
+//       {/*Sidebar div  closing*/}
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
 import React, { useState } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
@@ -62,22 +153,30 @@ const Layout = ({ children }) => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex flex-grow">
-        {isSidebarOpen && <Sidebar />}
-        <div className="flex flex-col flex-grow relative">
-          <Header
-            toggleSidebar={toggleSidebar}
-            toggleNotificationMenu={toggleNotificationMenu}
-          />
-          <div className="flex-grow p-4">
-            <div className="flex flex-grow">
-              {/* {isNotificationMenuOpen && <NotificationMenu />} */}
-              <div className="flex flex-col flex-grow relative">{children}</div>
-            </div>
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      {isSidebarOpen && <Sidebar />}
+
+      <div className="flex flex-col flex-1">
+        {/* Header */}
+        <Header
+          toggleSidebar={toggleSidebar}
+          toggleNotificationMenu={toggleNotificationMenu}
+        />
+
+        {/* Content */}
+        <div className="flex flex-1">
+          {/* Left Content */}
+          <div className="flex-1 p-4">{children}</div>
+
+          {/* Right Content */}
+          <div className="flex">
+            {isNotificationMenuOpen && <NotificationMenu />}
           </div>
-          <Footer />
         </div>
+
+        {/* Footer */}
+        <Footer />
       </div>
     </div>
   );
