62be66020b1c682c8e2fd0f9a6c84816b6fed7fc
Ankit Kumar Thakur <drankitkumarthakur@gmail.com>
2023-07-03 23:04:25 +0530

    finally most of the layout has been achieved

--- src/pages/FinalDashboard/Dashboard.js
+++ src/pages/FinalDashboard/Dashboard.js
@@ -1,3 +1,44 @@
+// import React from "react";
+// import InfoBox from "../../components/InfoBox/InfoBox";
+// import Pie from "../Pie";
+// import DashboardCategory from "../../data/dashboardData/DashboardCategory";
+// import RecentActivity from "../../components/RecentActivity/RecentActivity";
+// import AssetLocation from "../../components/AssetLocation/AssetLocation";
+// import UserCategory from "../../components/UserCategory/UserCategory";
+// // import NewUserCategory from "../../components/NewUserCategory/NewUserCategory";
+// // import DoughnutChartDemo from "../PieChart";
+
+// const Dashboard = () => {
+//   return (
+//     <div className="flex flex-col">
+//       <div>
+//         <InfoBox categories={DashboardCategory} title="Dashboard" />
+//       </div>
+//       <div className="flex mt-3">
+//         <div className="w-1/5 ml-2 rounded-lg shadow-md -mt-1 z-20 pt-6">
+//           <h3 className="text-start font-bold p-2 ml-2 text-2xl">
+//             Ticket Status
+//           </h3>
+//           <div className="-mb-4">{/* <Pie /> */}</div>
+//         </div>
+//         <div className="w-3/5 shadow-md ml-2 rounded-lg mb-5 py-1">
+//           <RecentActivity />
+//         </div>
+//       </div>
+//       <div className="flex mt-3">
+//         <div className="w-1/2 ml-2 rounded-lg shadow-md h-16 -mt-1 z-20">
+//           <AssetLocation />
+//         </div>
+//         <div className="w-1/2 shadow-md ml-2 rounded-lg mb-5">
+//           <UserCategory />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
 import React from "react";
 import InfoBox from "../../components/InfoBox/InfoBox";
 import Pie from "../Pie";
@@ -10,29 +51,24 @@ import UserCategory from "../../components/UserCategory/UserCategory";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col">
-      <div>
+    <div className="flex flex-col w-3/4">
+      <div className="flex-item-1">
         <InfoBox categories={DashboardCategory} title="Dashboard" />
       </div>
-      <div className="flex mt-3">
-        <div className="w-1/5 ml-2 rounded-lg shadow-md -mt-1 z-20 pt-6">
-          <h3 className="text-start font-bold p-2 ml-2 text-2xl">
-            Ticket Status
-          </h3>
-          <div className="-mb-4">
+      <div className="flex-item-2 mt-4">
+        <div class="flex-container-2 flex flex-row flex-wrap">
+          <div className="flex-item-2-1">
             <Pie />
           </div>
-        </div>
-        <div className="w-3/5 shadow-md ml-2 rounded-lg mb-5 py-1">
-          <RecentActivity />
-        </div>
-      </div>
-      <div className="flex mt-3">
-        <div className="w-1/2 ml-2 rounded-lg shadow-md h-16 -mt-1 z-20">
-          <AssetLocation />
-        </div>
-        <div className="w-1/2 shadow-md ml-2 rounded-lg mb-5">
-          <UserCategory />
+          <div className="flex-item-2-2 mt-2">
+            <RecentActivity />
+          </div>
+          <div className="flex-item-2-3 mt-10 mb-8">
+            <AssetLocation />
+          </div>
+          <div className="flex-item-2-4 mt-16 mb-6 w-full">
+            <UserCategory />
+          </div>
         </div>
       </div>
     </div>
